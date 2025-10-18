ec580f2c928a4401e45b43b0db5dbdf3971c6d91
sean <yepaik@calpoly.edu>
2025-10-17 18:11:28 -0700

    fixed backend setup

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
