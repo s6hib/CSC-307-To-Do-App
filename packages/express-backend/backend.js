94bbcdc1a96a827a80b855e2401e10145dbefaa0
ange1ma <siyinmaa@gmail.com>
2025-10-28 16:16:16 -0700

    format for post/delete methods

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -11,6 +11,25 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+app.post("/tasks", (req, res) => {
+  //const taskToAdd = ...;
+  // for tasks
+});
+
+app.delete("/tasks", (req, res) => {
+  // to delete tasks
+});
+
+app.post("/tasks/:subtasks", (req, res) => {
+  //const subtaskToAdd = ...;
+  // for subtasks within tasks
+});
+
+app.delete("/tasks/:subtasks", (req, res) => {
+  // to delete subtasks?
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
