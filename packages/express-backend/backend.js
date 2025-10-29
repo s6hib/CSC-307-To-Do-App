fe2df50ba9b617ed7367ee5787a54293c5db9e0b
ange1ma <siyinmaa@gmail.com>
2025-10-28 17:28:49 -0700

    add authentication logic for user/pwd

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const port = 8000;
@@ -11,6 +12,45 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// to generate an access token
+function generateAccessToken(username) {
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      { username: username },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "1d" },
+      (error, token) => {
+        if (error) reject(error);
+        else resolve(token);
+      }
+    );
+  });
+}
+
+// to authenticate user using jwt
+function authenticateUser(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    res.status(401).end();
+  } else {
+    jwt.verify(token, process.env.TOKEN_SECRET,
+      (error, decoded) => {
+        if (decoded) { next(); }
+        else { res.status(401).end(); }
+      }
+    );
+  }
+}
+  
+  
+// authenticate user
+app.post("/users", authenticateUser, (req, res) => {
+  const userToAdd = req.body;
+  Users.addUser(userToAdd).then((result) =>
+    res.status(201).send(result)
+  );
+});
 
 app.post("/tasks", (req, res) => {
   //const taskToAdd = ...;
