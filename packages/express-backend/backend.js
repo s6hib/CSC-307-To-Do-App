2e53feb0c9a4671de5ff1728d79c608076710eb2
ange1ma <siyinmaa@gmail.com>
2025-11-18 15:52:22 -0800

    adding frontend link to origin

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,7 +33,7 @@ const port = 8000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://nice-glacier-0ec51b010.3.azurestaticapps.net"],
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
 
