4f60c299699e8d020cf82f452be5a7af6d554633
ange1ma <siyinmaa@gmail.com>
2025-11-21 18:50:40 -0800

    testing date

--- packages/react-frontend/src/pages/components/Table.jsx
+++ packages/react-frontend/src/pages/components/Table.jsx
@@ -49,7 +49,7 @@ export default function Table({
             {/* due date on the right */}
             <div className="due">
               {new Date(row.date).toLocaleDateString("en-US", {
-                timeZone: "America/Los_Angeles",
+                timeZone: "UTC",
                 year: "numeric",
                 month: "short",
                 day: "numeric"
