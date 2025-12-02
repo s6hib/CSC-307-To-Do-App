f4d8597ecd983a33c4cad5b4d36f978b25229af9
sean <yepaik@calpoly.edu>
2025-12-01 21:28:34 -0800

    72%; FolderTasksPage test cases completed

--- packages/react-frontend/src/__test__/FolderTasksPage.test.jsx
+++ packages/react-frontend/src/__test__/FolderTasksPage.test.jsx
@@ -1,12 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
 
 import FolderTasksPage from "../pages/FolderTasksPage.jsx";
 
 //since we rendered FolderTasksPage directly, we have to put ToastProvider around it
+const mockShow = jest.fn();
 jest.mock("../pages/components/ToastProvider.jsx", () => ({
-  useToast: () => ({ show: jest.fn() })
+  useToast: () => ({ show: mockShow })
+}));
+
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate
 }));
 
 const renderFolderTasks = (route = "/folders/123") =>
@@ -24,6 +35,10 @@ const renderFolderTasks = (route = "/folders/123") =>
       </Routes>
     </MemoryRouter>
   );
+beforeEach(() => {
+  jest.clearAllMocks();
+  global.fetch = jest.fn();
+});
 
 const mockId123 = () => {
   fetch.mockResolvedValueOnce({
@@ -118,57 +133,256 @@ test("task page layout", async () => {
   ).toBeInTheDocument();
 });
 
-test("reroute to folders page", async () => {
-  mockId123();
-  const user = userEvent.setup();
-  await screen.findByText(/csc 307/i);
-  await user.click(
-    screen.getByRole("button", { name: /← back to folders/i })
-  );
+// test("reroute to folders page", async () => {
+//   const user = userEvent.setup();
+//   mockId123();
+//   await screen.findByText(/csc 307/i);
+//   await user.click(
+//     screen.getByRole("button", { name: /← back to folders/i })
+//   );
 
-  expect(
-    await screen.findByText(/folders page/i)
-  ).toBeInTheDocument();
-});
+//   expect(
+//     await screen.findByText(/folders page/i)
+//   ).toBeInTheDocument();
+// });
 
-test("add new task", async () => {
+// test("add new task", async () => {
+//   const user = userEvent.setup();
+//   fetch
+//     .mockResolvedValueOnce({
+//       ok: true,
+//       json: async () => []
+//     })
+//     .mockResolvedValueOnce({
+//       ok: true,
+//       json: async () => [{ _id: "123", name: "CSC 307" }]
+//     })
+//     //POST method when addTask() is called
+//     .mockResolvedValueOnce({
+//       ok: true,
+//       status: 201,
+//       json: async () => ({
+//         _id: "123",
+//         task: "Testing",
+//         date: "2025-12-13",
+//         done: false
+//       })
+//     });
+//   renderFolderTasks();
+//   await screen.findByText(/csc 307/i);
+//   await user.type(
+//     screen.getByLabelText(/task description/i),
+//     "Testing"
+//   );
+//   await user.type(screen.getByLabelText(/date/i), "2025-12-13");
+//   await user.click(
+//     screen.getByRole("button", { name: /\+ add task/i })
+//   );
+
+//   expect(
+//     await screen.findByText(/testing/i)
+//   ).toBeInTheDocument();
+//   expect(screen.getByText(/done/i)).toBeInTheDocument();
+//   expect(screen.getByText("Task")).toBeInTheDocument();
+//   expect(screen.getByText(/due date/i)).toBeInTheDocument();
+//   expect(screen.getByText(/actions/i)).toBeInTheDocument();
+// });
+
+test("create task", async () => {
   const user = userEvent.setup();
-  fetch
-    .mockResolvedValueOnce({
-      ok: true,
-      json: async () => []
+  fetch.mockResolvedValueOnce({
+    ok: true,
+    json: async () => []
+  });
+  fetch.mockResolvedValueOnce({
+    ok: true,
+    json: async () => [{ _id: "123", name: "CSC 307" }]
+  });
+  fetch.mockResolvedValueOnce({
+    status: 201,
+    json: async () => ({
+      _id: "1",
+      task: "test",
+      date: "2025-01-01",
+      done: false,
+      folder: "123"
     })
-    .mockResolvedValueOnce({
-      ok: true,
-      json: async () => [{ _id: "123", name: "CSC 307" }]
-    })
-    //POST method when addTask() is called
-    .mockResolvedValueOnce({
-      ok: true,
-      status: 201,
-      json: async () => ({
-        _id: "123",
-        task: "Testing",
-        date: "2025-12-13",
-        done: false
-      })
-    });
+  });
   renderFolderTasks();
+
   await screen.findByText(/csc 307/i);
   await user.type(
     screen.getByLabelText(/task description/i),
-    "Testing"
+    "test"
   );
-  await user.type(screen.getByLabelText(/date/i), "2025-12-13");
+  await user.type(screen.getByLabelText(/date/i), "2025-01-01");
   await user.click(
-    screen.getByRole("button", { name: /\+ add task/i })
+    screen.getByRole("button", {
+      name: /\+ add task/i
+    })
+  );
+  //check if POST method called
+  expect(fetch).toHaveBeenLastCalledWith(
+    "https://adder-backend.azurewebsites.net/api/tasks",
+    expect.objectContaining({
+      method: "POST",
+      credentials: "include"
+    })
   );
 
+  expect(await screen.findByText(/test/i)).toBeInTheDocument();
   expect(
-    await screen.findByText(/testing/i)
-  ).toBeInTheDocument();
-  expect(screen.getByText(/done/i)).toBeInTheDocument();
-  expect(screen.getByText("Task")).toBeInTheDocument();
-  expect(screen.getByText(/due date/i)).toBeInTheDocument();
-  expect(screen.getByText(/actions/i)).toBeInTheDocument();
+    screen.getByLabelText(/task description/i)
+  ).toHaveValue("");
+  expect(screen.getByLabelText(/date/i)).toHaveValue("");
+  //toast success message
+  expect(mockShow).toHaveBeenCalledWith(
+    "Task created",
+    "success"
+  );
+});
+
+test("failed to create task", async () => {
+  const user = userEvent.setup();
+  window.alert = jest.fn();
+  mockId123();
+  await screen.findByText(/csc 307/i);
+  const addButton = screen.getByRole("button", {
+    name: /\+ add task/i
+  });
+  await user.click(addButton);
+
+  expect(window.alert).toHaveBeenCalledWith(
+    "Please enter both task description and date"
+  );
+  expect(fetch).toHaveBeenCalledTimes(2);
+});
+
+test("mark task done", async () => {
+  const user = userEvent.setup();
+  const task = {
+    _id: "1",
+    task: "test",
+    date: "2025-01-01T00:00:00.000Z",
+    done: false
+  };
+  fetch.mockResolvedValueOnce({
+    ok: true,
+    json: async () => [task]
+  });
+  fetch.mockResolvedValueOnce({
+    ok: true,
+    json: async () => [{ _id: "123", name: "CSC 307" }]
+  });
+  fetch.mockResolvedValueOnce({
+    status: 200,
+    json: async () => ({ ...task, done: true })
+  });
+  renderFolderTasks();
+  await screen.findByText(/test/i);
+  //see if task is marked done already
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+  await user.click(checkbox);
+  //check if PUT method was made
+  expect(fetch).toHaveBeenLastCalledWith(
+    "https://adder-backend.azurewebsites.net/api/tasks/1",
+    expect.objectContaining({
+      method: "PUT"
+    })
+  );
+  //ensure the task is marked done
+  await waitFor(() => {
+    expect(checkbox).toBeChecked();
+  });
+});
+
+test("delete task", async () => {
+  const user = userEvent.setup();
+  const task = {
+    _id: "1",
+    task: "test",
+    date: "2025-01-01T00:00:00.000Z",
+    done: false
+  };
+  fetch.mockResolvedValueOnce({
+    ok: true,
+    json: async () => [task]
+  });
+  fetch.mockResolvedValueOnce({
+    ok: true,
+    json: async () => [{ _id: "123", name: "CSC 307" }]
+  });
+  fetch.mockResolvedValueOnce({
+    status: 204,
+    json: async () => ""
+  });
+  renderFolderTasks();
+
+  expect(await screen.findByText(/test/i));
+  const deleteButton = screen.getByRole("button", {
+    name: "Delete"
+  });
+  await user.click(deleteButton);
+  //ensure DELETE method was called
+  expect(fetch).toHaveBeenLastCalledWith(
+    "https://adder-backend.azurewebsites.net/api/tasks/1",
+    expect.objectContaining({
+      method: "DELETE"
+    })
+  );
+
+  await waitFor(() => {
+    expect(screen.queryByText(/test/i)).not.toBeInTheDocument();
+  });
+
+  expect(mockShow).toHaveBeenCalledWith(
+    "Task deleted",
+    "success"
+  );
+});
+
+test("delete task", async () => {
+  const user = userEvent.setup();
+  window.prompt = jest.fn().mockReturnValue("test2");
+  const task = {
+    _id: "1",
+    task: "test",
+    date: "2025-01-01T00:00:00.000Z",
+    done: false
+  };
+  fetch.mockResolvedValueOnce({
+    ok: true,
+    json: async () => [task]
+  });
+  fetch.mockResolvedValueOnce({
+    ok: true,
+    json: async () => [{ _id: "123", name: "CSC 307" }]
+  });
+  fetch.mockResolvedValueOnce({
+    status: 200,
+    json: async () => ({ ...task, task: "test2" })
+  });
+  renderFolderTasks();
+
+  expect(await screen.findByText(/test/i)).toBeInTheDocument();
+
+  const taskRow = await screen.findByText(/test/i);
+  await user.click(taskRow);
+  expect(window.prompt).toHaveBeenCalledWith(
+    "Edit task:",
+    "test"
+  );
+  expect(fetch).toHaveBeenLastCalledWith(
+    "https://adder-backend.azurewebsites.net/api/tasks/1",
+    expect.objectContaining({
+      method: "PUT"
+    })
+  );
+
+  expect(await screen.findByText(/test2/i)).toBeInTheDocument();
+  expect(mockShow).toHaveBeenCalledWith(
+    "Task updated",
+    "success"
+  );
 });
