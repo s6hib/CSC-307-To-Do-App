672a7fe81454799c6f064c657d9a08bff4c44741
sean <yepaik@calpoly.edu>
2025-12-04 15:54:31 -0800

    after deleting test cases and coverages for backend

--- packages/express-backend/__test__/health.test.js
+++
@@ -1,8 +0,0 @@
-//just testing
-import request from "supertest";
-import app from "../app.js";
-
-test("GET /health returns 200 and json", async () => {
-  const res = await request(app).get("/health");
-  expect(res.statusCode).toBe(200);
-});
